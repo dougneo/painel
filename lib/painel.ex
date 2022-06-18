a74d9c9047f8eb381609f8b10037698b8ed7de39
Douglas Silva <doug.neo@gmail.com>
2022-06-18 15:48:44 -0300

    first commit

---
+++ lib/painel.ex
@@ -0,0 +1,18 @@
+defmodule Painel do
+  @moduledoc """
+  Documentation for `Painel`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Painel.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/painel_test.exs
@@ -0,0 +1,8 @@
+defmodule PainelTest do
+  use ExUnit.Case
+  doctest Painel
+
+  test "greets the world" do
+    assert Painel.hello() == :world
+  end
+end
